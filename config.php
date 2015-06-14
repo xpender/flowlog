6ef02f6810dd40f3d886bf8bc44ec041293fb320
Marko Kercmar <m.kercmar@whow.net>
2015-06-14 19:22:35 +0200

    minor config change, add start & endtime, add fixes

--- config.php
+++ config.php
@@ -2,6 +2,8 @@
 date_default_timezone_set('Europe/Berlin');
 
 $config = array(
-    'region' => 'eu-west-1',
-    'LogGroupName' => 'FlowLog'
+    'Region' => 'eu-west-1',
+    'LogGroupName' => 'FlowLog',
+    'StartTime' => mktime(13, 10, 0, 6, 14, 2015) * 1000,
+    'EndTime' => mktime(13, 20, 0, 6, 14, 2015) * 1000,
     );
