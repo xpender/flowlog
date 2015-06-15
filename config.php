bd743517c07f267f45b977dbda6e098688d0d9df
Marko Kercmar <m.kercmar@whow.net>
2015-06-15 10:41:28 +0200

    fetch without start & endtime

--- config.php
+++ config.php
@@ -6,6 +6,8 @@ date_default_timezone_set('Europe/Berlin');
 $config = array(
     'Region' => 'eu-west-1',
     'LogGroupName' => 'FlowLog',
-    'StartTime' => mktime(13, 10, 0, 6, 14, 2015) * 1000,
-    'EndTime' => mktime(13, 20, 0, 6, 14, 2015) * 1000,
+    #'StartTime' => mktime(13, 10, 0, 6, 14, 2015) * 1000,
+    'StartTime' => false,
+    #'EndTime' => mktime(13, 20, 0, 6, 14, 2015) * 1000,
+    'EndTime' => false,
     );
